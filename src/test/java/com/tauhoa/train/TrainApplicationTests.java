980e71c407ee4ff649aa3b0bf5947adad2fb4f00
LeeWang4u <lequanghien4u@gmail.com>
2025-03-15 16:30:10 +0700

    init

---
+++ src/test/java/com/tauhoa/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tauhoa.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
