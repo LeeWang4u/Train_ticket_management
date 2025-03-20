f9699804b4b2533cef856c124e4890a7c4fb364b
LeeWang4u <lequanghien4u@gmail.com>
2025-03-20 23:34:34 +0700

    init services, controller, repositories

---
+++ src/main/java/com/tauhoa/train/services/PassengerService.java
@@ -0,0 +1,11 @@
+package com.tauhoa.train.services;
+
+import com.tauhoa.train.services.impl.IPassengerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PassengerService implements IPassengerService {
+
+}
