512ecfdfdc8d950ef086035945dd819a5176fe04
LeeWang4u <lequanghien4u@gmail.com>
2025-03-21 22:07:31 +0700

    add TripRepository, TripService

---
+++ src/main/java/com/tauhoa/train/services/TripService.java
@@ -0,0 +1,11 @@
+package com.tauhoa.train.services;
+
+import com.tauhoa.train.services.impl.ITrainService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TripService implements ITrainService {
+
+}
