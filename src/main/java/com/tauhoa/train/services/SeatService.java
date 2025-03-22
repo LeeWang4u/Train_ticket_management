b34dd6c9cce4978c3b2396e456f11f2eafe3e67b
LeeWang4u <lequanghien4u@gmail.com>
2025-03-22 23:43:10 +0700

    add trip service

--- src/main/java/com/tauhoa/train/services/SeatService.java
+++ src/main/java/com/tauhoa/train/services/SeatService.java
@@ -1,10 +1,25 @@
 package com.tauhoa.train.services;
 
+import com.tauhoa.train.models.Seat;
+import com.tauhoa.train.repositories.SeatRepository;
 import com.tauhoa.train.services.impl.ISeatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class SeatService implements ISeatService {
+    private final SeatRepository seatRepository;
+    @Override
+    public Optional<Seat> getSeat(int id) {
+        return seatRepository.findById(id);
+    }
+
+    @Override
+    public List<Seat> findAllSeatByIdTrip(int idTrip) {
+        return seatRepository.findAllSeatsByTripId(idTrip);
+    }
 }
