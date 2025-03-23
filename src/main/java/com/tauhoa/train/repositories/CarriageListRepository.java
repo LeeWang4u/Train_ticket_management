cbee8fdcf7026b1c7ad62e8b3709623a0b073fa2
LeeWang4u <lequanghien4u@gmail.com>
2025-03-23 14:38:11 +0700

    get carriage list

--- src/main/java/com/tauhoa/train/repositories/CarriageListRepository.java
+++ src/main/java/com/tauhoa/train/repositories/CarriageListRepository.java
@@ -4,5 +4,12 @@ import com.tauhoa.train.models.CarriageList;
 import com.tauhoa.train.models.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CarriageListRepository extends JpaRepository<CarriageList, Integer> {
+    List<CarriageList> findAllByTrip(int trip_id);
+
+    @Override
+    Optional<CarriageList> findById(Integer integer);
 }
--- src/main/java/com/tauhoa/train/services/CarriageListService.java
+++ src/main/java/com/tauhoa/train/services/CarriageListService.java
@@ -1,10 +1,26 @@
 package com.tauhoa.train.services;
 
+import com.tauhoa.train.models.CarriageList;
+import com.tauhoa.train.repositories.CarriageListRepository;
+import com.tauhoa.train.services.impl.ICarriageListService;
 import com.tauhoa.train.services.impl.ICompartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class CarriageListService implements ICompartmentService {
+public class CarriageListService implements ICarriageListService {
+    private final CarriageListRepository carriageListRepository;
+    @Override
+    public Optional<CarriageList> getSeatById(int id) {
+        return carriageListRepository.findById(id);
+    }
+
+    @Override
+    public List<CarriageList> findAllCarriageListByIdTrip(int idTrip) {
+        return carriageListRepository.findAllByTrip(idTrip);
+    }
 }
