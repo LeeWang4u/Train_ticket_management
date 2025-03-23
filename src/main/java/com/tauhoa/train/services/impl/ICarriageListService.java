89bc8fc9737c1b6836d0b3788b785750e7c82e68
LeeWang4u <lequanghien4u@gmail.com>
2025-03-23 14:38:11 +0700

    get carriage list

--- src/main/java/com/tauhoa/train/services/impl/ICarriageListService.java
+++ src/main/java/com/tauhoa/train/services/impl/ICarriageListService.java
@@ -1,5 +1,14 @@
 package com.tauhoa.train.services.impl;
 
 
+import com.tauhoa.train.models.CarriageList;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
 public interface ICarriageListService {
+    Optional<CarriageList> getSeatById(int id);
+
+    List<CarriageList> findAllCarriageListByIdTrip(int idTrip);
 }
