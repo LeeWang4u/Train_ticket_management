af5ba63c55a8e5e2f0b0e1e2b386fb6cf89bb24a
LeeWang4u <lequanghien4u@gmail.com>
2025-03-23 23:32:40 +0700

    get trip by date in controller

--- src/main/java/com/tauhoa/train/controllers/TripController.java
+++ src/main/java/com/tauhoa/train/controllers/TripController.java
@@ -1,9 +1,32 @@
 package com.tauhoa.train.controllers;
 
+import com.tauhoa.train.models.Trip;
+import com.tauhoa.train.services.TripService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/timve")
 public class TripController {
+    @Autowired
+    TripService tripService;
+
+//    @PostMapping("")
+//    public ResponseEntity<?>
+
+    @GetMapping("/ketqua")
+    public ResponseEntity<?> getTripByDate(@PathVariable
+                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                                LocalDate tripDate){
+        List<Trip> trips = tripService.findByTripDate(tripDate);
+        return ResponseEntity.ok(trips);
+    }
+
 }
