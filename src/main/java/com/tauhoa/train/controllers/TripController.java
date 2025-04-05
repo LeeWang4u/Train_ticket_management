b3de6241ef1ae186d024b73dc1b87b08a4ba1def
LeeWang4u <lequanghien4u@gmail.com>
2025-04-05 14:30:14 +0700

    add get seat, carriage by tripis, departureStationName,  arrivalStationName

--- src/main/java/com/tauhoa/train/controllers/TripController.java
+++ src/main/java/com/tauhoa/train/controllers/TripController.java
@@ -64,7 +64,7 @@ public class TripController {
         try {
             List<TripResponseDTO> trips = tripService.findTripsByStationsAndDate(request);
             if (trips.isEmpty()) {
-                return ResponseEntity.status(404).body("No trips found for the given stations and date");
+                return ResponseEntity.status(200).body("No trips found for the given stations and date");
             }
             return ResponseEntity.ok(trips);
         } catch (IllegalArgumentException e) {
