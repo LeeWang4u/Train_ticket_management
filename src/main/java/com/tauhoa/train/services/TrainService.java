ce87427040fa357abd3c72ce23ba704dfa1b4a27
locle30 <loctv12011@gmail.com>
2025-04-29 15:11:39 +0700

    sche

--- src/main/java/com/tauhoa/train/services/TrainService.java
+++ src/main/java/com/tauhoa/train/services/TrainService.java
@@ -1,7 +1,12 @@
 package com.tauhoa.train.services;
 
+import com.tauhoa.train.models.Train;
+import com.tauhoa.train.repositories.TrainRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
