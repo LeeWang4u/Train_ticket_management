04f36d7d9b8172460ce8d348e2e53d0f8d467624
LeeWang4u <lequanghien4u@gmail.com>
2025-04-29 18:19:45 +0700

    fix api station

--- src/main/java/com/tauhoa/train/controllers/StationController.java
+++ src/main/java/com/tauhoa/train/controllers/StationController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ import java.util.Optional;
 //@RequiredArgsConstructor
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class StationController {
 
     private  final StationService stationService;
