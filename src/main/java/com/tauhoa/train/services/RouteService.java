69d1947d60792ce47541ffb78c480550f0384fb9
LeeWang4u <lequanghien4u@gmail.com>
2025-05-05 21:49:19 +0700

    add: add get all route

--- src/main/java/com/tauhoa/train/services/RouteService.java
+++ src/main/java/com/tauhoa/train/services/RouteService.java
@@ -1,10 +1,29 @@
 package com.tauhoa.train.services;
 
+import com.tauhoa.train.dtos.response.RouteResponseDTO;
+import com.tauhoa.train.dtos.response.TrainResponseDTO;
+import com.tauhoa.train.models.Route;
+import com.tauhoa.train.models.Train;
+import com.tauhoa.train.repositories.RouteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class RouteService implements IRouteService {
+    private final RouteRepository routeRepository;
 
+    @Override
+    public List<RouteResponseDTO> getAllRoute() {
+        List<Route> routes =   routeRepository.findAll();
+
+        return routes.stream()
+                .map(route -> new RouteResponseDTO(
+                        route.getRouteId(),
+                        route.getRouteName())) // Ánh xạ các trường tương ứng
+                .collect(Collectors.toList());
+    }
 }
